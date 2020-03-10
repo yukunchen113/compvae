8d79e003bd682b3e7ec23321642974e99eaa21e3
ychen <ychen@boomer>
2020-03-10 01:19:49 -0400

    test

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
+## Setup:
+Requires my libraty repo, please add that to your PYTHONPATH, such that the utils file can be used.
+
 ### Research Journal
 Here are some research notes.
 
-#### Experiment 1: Train BetaTCVAE, Create Mask by Randomizing Latents
+#### Experiment 1: Train BetaTCVAE, Create Mask by Randomizing Latents (Current)
 This experiment will try to create a mask by randomizing nuisance latent variables. These should randomize latents according to correlation between the latent variable of interest.
 There should be a degree of correlation between all of these variables since we are focusing on data that contain one object.
+
+We can make the mask by subtracting with original generated image, an applying a proportional gaussian filter onto the image, creating a mask
--- model.py
+++ model.py
@@ -1,7 +1,22 @@
-import utils as ut
+from utils.tf_custom.architectures.variational_autoencoder import VariationalAutoencoder
+from utils.other_library_tools.disentanglementlib_tools import gaussian_log_density, total_correlation 
+from utils.tf_custom.loss import kl_divergence_with_normal
 
-inputs, _ = ut.dataset.get_celeba_data(ut.general_constants.datapath, group_num=2)
+class CompVAE(VariationalAutoencoder):
+	def __init__(self, beta, name="BetaTCVAE", **kwargs):
+		super().__init__(name=name, **kwargs)
+		self.create_encoder_decoder_512() # use the larger model
+		self.beta = beta
 
-print(inputs.shape)
+	def call(self, inputs, m_sampled, m_logvar, m_mean):
+		sample, mean, logvar = self.encoder(inputs)
+		reconstruction = self.decoder(sample)
+		self.add_loss(self.regularizer(sample, mean, logvar, 
+				m_sampled, m_logvar, m_mean))
+		return reconstruction
 
-ut.BetaTCVAE()
+	def regularizer(self, sample, mean, logvar, m_sampled, m_logvar, m_mean):
+		# regularization uses disentanglementlib method
+		kl_loss = kl_divergence_with_normal(mean, logvar)
+		tc = (self.beta - 1) * total_correlation(sample, mean, logvar)
+		return tc + kl_loss
